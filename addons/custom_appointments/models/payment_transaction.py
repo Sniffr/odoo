b85dce9bde2cdc4b7de63e5ab47a329a8a9c8946
Sniffr <malingu.sidney@studnets.jkuat.ac.ke>
2025-11-07 08:46:14 +0300

    Revert "Fix payment transaction appointment linking for M-Pesa and PesaPal"
    This reverts commit a4926f46

--- addons/custom_appointments/models/payment_transaction.py
+++ addons/custom_appointments/models/payment_transaction.py
@@ -1,4 +1,4 @@
-from odoo import models, fields, api
+from odoo import models, fields
 
 
 class PaymentTransaction(models.Model):
@@ -6,11 +6,7 @@ class PaymentTransaction(models.Model):
     
     appointment_id = fields.Many2one('custom.appointment', string='Appointment', compute='_compute_appointment_id', store=True)
     
-    @api.depends('id')
     def _compute_appointment_id(self):
         for transaction in self:
-            if transaction.id:
-                appointment = self.env['custom.appointment'].search([('payment_transaction_id', '=', transaction.id)], limit=1)
-                transaction.appointment_id = appointment.id if appointment else False
-            else:
-                transaction.appointment_id = False
+            appointment = self.env['custom.appointment'].search([('payment_transaction_id', '=', transaction.id)], limit=1)
+            transaction.appointment_id = appointment.id if appointment else False
